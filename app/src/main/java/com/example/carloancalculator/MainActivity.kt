9605b477501bb92970d9cb56b45f76bd5e7193f1
LOSHENE <loshenes-wg17@student.tarc.edu.my>
2019-11-11 23:56:40 +0800

    commit

--- app/src/main/java/com/example/carloancalculator/MainActivity.kt
+++ app/src/main/java/com/example/carloancalculator/MainActivity.kt
@@ -4,37 +4,77 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java .util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonCalculate.setOnClickListener(){
+            calculateLoan();
+        }
+        buttonreset.setOnClickListener(){
+            reset();
+        }
     }
-    buttonCalculate.setOnClickListener{
-        calculateLoan()
-    }
+          
 
-    private fun calculateLoan(){ //calculate car loan
+
+
+    private fun calculateLoan(){ //calculate car loan //user inpput and output
+
+
 
         if(editTextcarPrice.text.isEmpty())
         {
             editTextcarPrice.setError(getString(R.string.input_error))
+            return
         }
-        val carPrice:Float=editTextcarPrice.text.toString().toFloat()
-        val down_payment :Int =editTextDownPayment.text.toString().toInt()
-        val interest_rate :Float = editTextInterestRate.text.toString().toFloat()
-        val monthly_payment : Int =monthlypayment.text.toString().toInt()
+        if(editTextDownPayment.text.isEmpty()){
+            editTextDownPayment.setError(getString(R.string.input_error))
+            return
+        }
+        if(editTextInterestRate.text.isEmpty()){
+            editTextInterestRate.setError(getString(R.string.input_error))
+            return
+        }
+        if(editTextloanPeriod.text.isEmpty()){
+            editTextloanPeriod.setError(getString(R.string.input_error))
+        }
+        val carPrice: Int =editTextcarPrice.text.toString().toInt();
+        val downPayment :Int =editTextDownPayment.text.toString().toInt()
+        val interestRate :Float = editTextInterestRate.text.toString().toFloat()
+        val loanPeriod : Int=editTextloanPeriod.text.toString().toInt();
 
-        val interest = carLoan * interest_rate * editTextloanPeriod
+        val CarLoan:Int = carPrice - downPayment;
+        val Interest : Float = CarLoan * interestRate *loanPeriod;
+        val monthlyPayment= (CarLoan + Interest )/loanPeriod / 12 ;
+
+        val currency : Currency = Currency.getInstance(Locale.getDefault());
+
+        CarLoan.text= String.format("%s %s %d" ,"loan : ",currency,CarLoan )
+        Interest.text= String.format("%s %s %2f","interest : " ,currency,Interest)
+        monthlypayment.text= String.format("%s %s %2f","Monthly Payment : " ,currency,monthlyPayment)
+
+
+
 
     }
-    fun reset(view: View){
+    private fun reset(){
+        editTextloanPeriod.text.clear();
+        editTextInterestRate.text.clear();
+        editTextDownPayment.text.clear();
+        editTextcarPrice.text.clear();
+
+
+        CarLoan.text ="";
+
 
 
     }
 
-    v
+
 
 
 }
